68d51a8a689e3efd1835c33528cf6c877460c8a0
DPritchard29 <102044905+DPritchard29@users.noreply.github.com>
2022-03-21 16:47:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+import os
 
 dataset = 'https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv'
 df = pd.read_csv(dataset)
 
-st.dataframe(df)
+df['Order Date'] = pd.to_datetime(df['Order Date'], format='%m/%d/%Y')
+graph = df.groupby(df['Order Date'].dt.month)['Sales'].sum()
 
+st.dataframe(graph)
