03df3c1ec772bb92e7f28a689366b2878e6dce2d
DPritchard29 <102044905+DPritchard29@users.noreply.github.com>
2022-03-29 16:07:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,7 @@ graph = df.groupby(df['Order Date'].dt.month)['Sales'].sum()
 
 st.dataframe(graph)
 st.line_chart(data=graph, width=0, height=0, use_container_width=True)
+
+dataset2= 'https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv'
+df2 = pd.read_csv(dataset2)
+graph2 = df2.groupby('Best Pos')[['Was_Injured?']].mean().sort_values(['Was_Injured?'], ascending = [False])
