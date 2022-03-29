05d1a2b7ab4a6d864e9fc805730ed7f430e0e5dc
DPritchard29 <102044905+DPritchard29@users.noreply.github.com>
2022-03-29 16:20:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import datetime
 
-
+'''
 dataset = 'https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv'
 df = pd.read_csv(dataset)
 
@@ -12,6 +12,8 @@ graph = df.groupby(df['Order Date'].dt.month)['Sales'].sum()
 
 st.dataframe(graph)
 st.line_chart(data=graph, width=0, height=0, use_container_width=True)
+'''
+
 
 dataset2 = 'dataset/Players_Combined_Cleaned.csv'
 df2 = pd.read_csv(dataset2)
