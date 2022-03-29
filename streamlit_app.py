acd7ac416617f2a7006f1fe934baa12b0651af90
DPritchard29 <102044905+DPritchard29@users.noreply.github.com>
2022-03-29 16:21:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,17 @@ import pandas as pd
 import datetime
 
 '''
-dataset = 'https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv'
-df = pd.read_csv(dataset)
+dataset2 = 'https://raw.githubusercontent.com/jeaggo/tc3068/master/Superstore.csv'
+df2 = pd.read_csv(dataset2)
 
-df['Order Date'] = pd.to_datetime(df['Order Date'], format='%m/%d/%Y')
-graph = df.groupby(df['Order Date'].dt.month)['Sales'].sum()
+df2['Order Date'] = pd.to_datetime(df2['Order Date'], format='%m/%d/%Y')
+graph2 = df2.groupby(df2['Order Date'].dt.month)['Sales'].sum()
 
-st.dataframe(graph)
-st.line_chart(data=graph, width=0, height=0, use_container_width=True)
+st2.dataframe(graph2)
+st.line_chart(data=graph2, width=0, height=0, use_container_width=True)
 '''
 
 
-dataset2 = 'dataset/Players_Combined_Cleaned.csv'
-df2 = pd.read_csv(dataset2)
-st.dataframe(data=df2, width=None, height=None)
+dataset = 'dataset/Players_Combined_Cleaned.csv'
+df = pd.read_csv(dataset)
+st.dataframe(data=df, width=None, height=None)
