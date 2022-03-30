4dbee5b94036b090cb4e6049985c478fbf1e6b97
DPritchard29 <102044905+DPritchard29@users.noreply.github.com>
2022-03-30 10:42:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,37 +4,6 @@ import pandas as pd
 import datetime
 
 
-dataset = 'dataset/Players_Combined_Cleaned.csv'
-df = pd.read_csv(dataset)
-st.dataframe(data=df, width=None, height=None)
-
-X = df[df.columns]
-X = sm.add_constant(X)
-y = df['Was_Injured?']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-
-Injured = df[df['Was_Injured?'] == 1]
-Not_Injured = df[df['Was_Injured?'] == 0]
-
-cols = [
-       'Age',
-       'Work_Rate', '% Games Played When Fit', 'Club_Arsenal',
-       'Club_Aston Villa', 'Club_Brighton', 'Club_Burnley', 'Club_Chelsea',
-       'Club_Crystal Palace', 'Club_Everton', 'Club_Fulham',
-       'Club_Leeds United', 'Club_Leicester City', 'Club_Liverpool FC',
-       'Club_Manchester City', 'Club_Manchester United',
-       'Club_Newcastle United', 'Club_Sheffield United', 'Club_Southampton',
-       'Club_Tottenham Hotspur', 'Club_West Bromwich Albion',
-       'Club_West Ham United', 'Club_Wolverhampton Wanderers', 'Best Pos_CM',
-       'Best Pos_GK', 'Best Pos_LD', 'Best Pos_LM', 'Best Pos_RD',
-       'Best Pos_RM', 'Best Pos_ST', 'Best Pos_CD']
-
-lg_reg = sm.Logit(y_train, X_train[cols])
-results = lg_reg.fit()
-X_train['target_pred'] = results.predict(X_train[cols])
-
-st.dataframe(data=X_train, width=None, height=None)
-
 player_info = {"Age": [20],
               "Work_Rate": [15],
               "% Games Played When Fit": [0.8],
@@ -68,18 +37,3 @@ player_info = {"Age": [20],
               "Best Pos_CD": [0]
               }
 
-def injury_calculator(player_info):
-    df = pd.DataFrame(player_info)
-    df['target_pred'] = results.predict(df[cols])
-    if df['target_pred'][0] > 0.75:
-        risk = 'High risk'
-    elif df['target_pred'][0] > 0.57:
-        risk = 'Medium risk'
-    else:
-        risk = 'Low risk'
-    
-    expected_games_missed = int(round((2.718**((df['target_pred'][0])*-1))*20, 0))
-    print(f'Expected games missed = {expected_games_missed}')
-    print(f'This player is = {risk}')
-
-injury_calculator(player_info)
