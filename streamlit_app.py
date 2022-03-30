8f7ad71b9deef8a617cb36460f62a485a0f685eb
DPritchard29 <102044905+DPritchard29@users.noreply.github.com>
2022-03-30 11:11:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,10 +47,8 @@ elif pred_calc > 0.57:
   risk = 'Medium risk'
 else:
   risk = 'Low risk'
-   
-expected_games_missed = int((2.718**((pred_calc*(-1)))*20)
-
+ 
 
 st.text(player_info['Age'])
 st.text(risk)
-st.text(expected_games_missed) 
+st.text('HEllO) 
